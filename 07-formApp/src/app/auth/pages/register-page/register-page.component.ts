ba88c75e97a799124c4f9fe20fed42593466e57c
Enrique Martinez <ecm.bolano@gmail.com>
2023-08-20 18:22:51 -0500

    Avance curso angular

--- 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/service/validators.service';
+//import * as customValidator from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern )  ]],
+    // email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern ) ], [ new EmailValidator() ]], // Se comentarea para dejarla como referencia Video 252. Validaciones asíncronas 9:13
+    email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern ) ], [ this.emailValidator ]],
+    userName: ['', [ Validators.required, this.validatorsService.cantBeStrider ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]],
+  })
+
+  constructor( 
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidator
+    ){}
+
+  isValidField( field: string ){
+     return this.validatorsService.isValiedField( this.myForm, field );
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 
 }
---
+++ 07-formApp/src/app/shared/service/validators.service.ts
@@ -0,0 +1,32 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, ValidationErrors } from '@angular/forms';
+
+@Injectable({ providedIn: 'root' })
+export class ValidatorsService {
+
+    public firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+    public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+    public cantBeStrider = ( control: FormControl ): ValidationErrors | null => {
+
+
+
+        const value: string = control.value.trim().toLowerCase();
+
+        if ( value === 'strider' ) {
+
+            return {
+                noStrider: true,
+            }
+        }
+
+        return null;
+    }
+
+    public isValiedField( form: FormGroup, field: string ) {
+        return form.controls[field].errors && form.controls[field].touched;      
+    }
+
+}
