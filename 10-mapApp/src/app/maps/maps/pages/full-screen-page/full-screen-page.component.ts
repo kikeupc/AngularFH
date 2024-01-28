70126f38c46d6d46d85da0eed4863eb5d171ad68
Enrique Martinez <ecm.bolano@gmail.com>
2024-01-28 15:29:31 -0500

    avance del curso

--- 10-mapApp/src/app/maps/maps/pages/full-screen-page/full-screen-page.component.ts
+++ 10-mapApp/src/app/maps/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+(mapboxgl as any).accessToken = 'pk.eyJ1Ijoia2lrZXVwYyIsImEiOiJjbHJmbjM2Y3EwMzltMmtteG50OTJrMmdzIn0.P9bAP8iIoYphMe6jYMGlLQ';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
   styleUrls: ['./full-screen-page.component.css']
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 
 }
---
+++ 10-mapApp/src/environments/environment.ts
@@ -0,0 +1,5 @@
+
+export const environment = {
+  mapbox_key: "pk.eyJ1Ijoia2lrZXVwYyIsImEiOiJjbHJmbjM2Y3EwMzltMmtteG50OTJrMmdzIn0.P9bAP8iIoYphMe6jYMGlLQ",
+  otra: "PROPIEDAD",
+};
